d61154baa9133c8a0fe9e8e8bb0b34dcbfda859c
cedric e goddard <8ceg6@comcast.net>
2019-12-19 19:58:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect 
+  name_hash.collect |name, num|
+  
 binding.pry
 end
